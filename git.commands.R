246c040b139f9edd15456fecf6365b96dc393b41
ericaeturner <ericaeturner@gmail.com>
2020-04-26 20:42:10 -0400

    updating plots

--- git.commands.R
+++ git.commands.R
@@ -2,6 +2,15 @@ cd /Users/Erica/Desktop/Data-Science/final.project
 git init
 touch .gitignore
 echo "*.csv"  >>.gitignore
+echo "*.cpg"  >>.gitignore
+echo "*.dbf"  >>.gitignore
+echo "*.prj"  >>.gitignore
+echo "*.shp"  >>.gitignore
+echo "*.shx"  >>.gitignore
+echo "*.xml"  >>.gitignore
+
+
+
 git add .
 git remote add origin https://github.com/ericaeturner/DC.crime.and.infrastructure.git
 git commit -am "first commit"
